5df86613b6c4bd88cf69045592e8ecc8fc37a188
Micah Banschick <micha.ban@gmail.com>
2020-03-18 17:16:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, word) {
+  let arr = [];
+  for (let i = 0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`)
+  }
+  return arr;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i)
+  }
+}
