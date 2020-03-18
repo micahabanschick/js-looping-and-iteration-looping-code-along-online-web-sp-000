2483d5fd187a237a37cf360b3540b3a9196dedcd
Micah Banschick <micha.ban@gmail.com>
2020-03-18 17:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
 function writeCards(names, word) {
   let arr = [];
-  for (let i = 0; i < names.length(); i++) {
-    arr.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`)
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${word} gift!`)
   }
   // let i = 0;
   // while (i < names.length) {
