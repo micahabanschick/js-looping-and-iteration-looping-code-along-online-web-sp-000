a5b45d0408b82759e913634921d26d5f13c44781
Micah Banschick <micha.ban@gmail.com>
2020-03-18 16:49:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = `message for ${arr}.`
+  }
+}
+
+function countdown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(num)
+  }
+}
