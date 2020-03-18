b68bf8adc1c64abd77bbebd5b30e409c7822e1d8
Micah Banschick <micha.ban@gmail.com>
2020-03-18 16:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 function writeCards(arr) {
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = `message for ${arr}.`
+    arr[i] = `Thank you, ${arr}, for the wonderful surprise gift!`
   }
 }
-
+[ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ]
 function countDown(num) {
   for (let i = num; i >= 0; i--) {
     console.log(i)
